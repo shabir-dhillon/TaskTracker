27ade39b8b810d2763aaf9f76ee78ed646b110b2
shabir-dhillon <shabirsingh00@gmail.com>
2021-03-04 06:13:34 +0800

    first commit, app complete, no backend

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+import Button from './Button'
+
+const Header = ({ onAdd, showAdd }) => {
+
+  return (
+    <header className="header">
+      <h1>Task Tracker</h1>
+      <Button color={showAdd ? 'steelblue' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd} />
+    </header>
+  )
+}
+
+
+export default Header
